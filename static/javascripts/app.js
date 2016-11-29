1aeee1f5929a279c02574f91e0269912a12c5005
Doris Leong <dleong@csu.fullerton.edu>
2016-11-28 18:28:26 -0800

    Update knockout viewmodel and data binds

--- static/javascripts/app.js
+++ static/javascripts/app.js
@@ -6,23 +6,33 @@ var main = function () {
   var socket = io.connect('http://localhost:3000/');
   var userList = [];
   var answered = false;
+  var currentScoreText = 'Right: 0, Wrong: 0';
+
+  //Knockout
+
+  var ViewModel = function (userList, totalScore) {
+    this.users = ko.observableArray(userList); // Initial users
+    this.score = ko.observable(totalScore);
+  };
+
+  ko.applyBindings(new ViewModel(userList, currentScoreText));
 
   //Update the current list of players when one connects/disconnects
-  socket.on('update', function (users){
+  socket.on('update', function (users) {
     userList = users;
-    $('#onlineUsers').empty();
-    for(var i=0; i<userList.length; i++) {
-      if (userList[i].username != null) {
-        $('#onlineUsers').append('<li class="list-group-item">' + userList[i].username + '</li>'); 
-      }
-    }
+    // $('#onlineUsers').empty();
+    // for (var i = 0; i < userList.length; i++) {
+    //   if (userList[i].username != null) {
+    //     $('#onlineUsers').append('<li class="list-group-user">' + userList[i].username + '</li>');
+    //   }
+    // }
   });
-  
+
   var postAJAX = function (url, data, successFunction) {
     $.ajax({
       url: url,
       dataType: 'json',
-      type: 'POST', 
+      type: 'POST',
       data: data,
       contentType: 'application/json',
       success: function (response) {
@@ -48,12 +58,14 @@ var main = function () {
   //Send guess to server returns if correct (POST /answer)
   var postGuess = function () {
     var guess = $('#guess').val();
-    postAJAX('/answer',JSON.stringify({answerId: currentAnswerId, answer: guess}), function(response){
+    postAJAX('/answer', JSON.stringify({
+      answerId: currentAnswerId,
+      answer: guess
+    }), function (response) {
       currentAnswer = response.answer;
       if (response.correct === true) {
         $('#result').text('Correct!');
-      }
-      else {
+      } else {
         $('#result').text('Wrong!');
       }
       $('#inputGuess').prop('disabled', true);
@@ -64,7 +76,10 @@ var main = function () {
   var postQuestion = function () {
     var questionInput = $('#newQuestion').val();
     var answerInput = $('#newAnswer').val();
-    postAJAX('/question',JSON.stringify({question:questionInput, answer:answerInput}), function(response){
+    postAJAX('/question', JSON.stringify({
+      question: questionInput,
+      answer: answerInput
+    }), function (response) {
       $('#addConfirmation').text(response.confirm);
     });
     $('#newQuestion').val('');
@@ -79,7 +94,8 @@ var main = function () {
       type: 'GET',
       contentType: 'application/json',
       success: function (response) {
-        $('.score').text('Right: '+ response.right+', Wrong: '+response.wrong);
+        //$('.score').text('Right: ' + response.right + ', Wrong: ' + response.wrong);
+        currentScoreText = 'Right: ' + response.right + ', Wrong: ' + response.wrong;
       }
     });
   }
@@ -88,16 +104,16 @@ var main = function () {
   $('.game').hide();
   $('.round').hide();
   $('.create').hide();
-  $('#createShow').click(function() {
+  $('#createShow').click(function () {
     $('.create').toggle();
   });
 
-  $('#inputUsername').click(function() {
+  $('#inputUsername').click(function () {
     //check if no socket user with same name
     var username = $('#username').val();
     var taken = false;
 
-    for (var i=0; i< userList.length; i++) {
+    for (var i = 0; i < userList.length; i++) {
       if (userList[i].username == username) {
         $('#usernameError').text('Username taken');
         taken = true;
@@ -112,19 +128,19 @@ var main = function () {
     }
   });
 
-  $('#start').click(function() {
+  $('#start').click(function () {
     $('.round').show();
     $('#start').hide();
     socket.emit('play');
   });
 
-  $('#inputGuess').click(function() {
+  $('#inputGuess').click(function () {
     postGuess();
     answered = true;
     getScore();
   })
 
-  $('#addQuestion').click(function() {
+  $('#addQuestion').click(function () {
     postQuestion();
   });
 
@@ -138,15 +154,16 @@ var main = function () {
     getQuestion();
   });
 
-  socket.on('gameStart', function() {
+  socket.on('gameStart', function () {
     answered = false;
     var count = 15;
-    
+
     var counter = setInterval(timer, 1000);
     $('#inputGuess').prop('disabled', false);
     $('.round').show();
     $('#start').hide();
     $('#question').text(currentQuestion);
+
     function timer() {
       $('.roundTime').text(count);
       count -= 1;
@@ -160,7 +177,7 @@ var main = function () {
       }
     }
   });
-  
+
 }; //End of main
 
 $(document).ready(main);
